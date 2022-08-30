b166291d52c094ca3c319ef85d134e5fb5281344
IvaTsu <ivan.tsud@1337.tech>
2022-08-30 11:25:34 +0200

    feat: implement '/auth/login' route

---
+++ src/modules/auth/auth.js
@@ -0,0 +1,28 @@
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const { compareSync } = require('bcryptjs');
+const { v4: uuidv4 } = require('uuid');
+const jwt = require('jsonwebtoken');
+
+const { find } = require('../../services/user');
+const config = require('../../config');
+
+const router = new Router();
+
+router.post('/login', bodyParser(), async (ctx) => {
+  const { login, password } = ctx.request.body;
+  const user = await find({ login });
+  if (!user || !compareSync(password, user.password)) {
+    const error = new Error();
+    error.status = 403;
+    throw error;
+  }
+
+  const refreshToken = uuidv4();
+  ctx.body = {
+    token: jwt.sign({ id: user.id }, config.secret),
+    refreshToken,
+  };
+});
+
+module.exports = router;
