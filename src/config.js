e09f804d102dc0c542255ed3073b37da13aa139e
IvaTsu <ivan.tsud@1337.tech>
2022-08-30 11:24:17 +0200

    feat: add 'JWT_SECRET'

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
-const rc = require("rc");
+const rc = require('rc');
 
-module.exports = rc("JWT", {
+module.exports = rc('JWT', {
   port: process.env.PORT || 3000,
-  connection: "./data",
+  connection: './data',
+  secret: process.env.JWT_SECRET,
 });
