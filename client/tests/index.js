ba430ecebd3a03ab95dd406d4ee1f1e516a6a38d
IvaTsu <ivan.tsud@1337.tech>
2023-01-09 11:23:22 +0100

    test: add 'Logout removes token information' test case

--- client/tests/index.js
+++ client/tests/index.js
@@ -30,3 +30,28 @@ test("Login captures token information", async (t) => {
     `Bearer ${LOGIN_RESPONSE.token}`,
   );
 });
+
+test("Logout removes token information", async (t) => {
+  const client = axios.create();
+  const mock = new MockAdapter(client);
+  const api = new Api({ client });
+
+  const LOGIN_REQUEST = {
+    login: "login",
+    password: "password",
+  };
+  const LOGIN_RESPONSE = {
+    token: "TOKEN",
+    refreshToken: "REFRESH_TOKEN",
+  };
+
+  mock.onPost("/auth/login", LOGIN_REQUEST).reply(200, LOGIN_RESPONSE);
+  mock.onGet("/users").reply(200, []);
+  await api.login(LOGIN_REQUEST);
+  api.logout();
+  await api.getUsers();
+
+  // verify that GET /users was called
+  t.is(mock.history.get.length, 1);
+  t.falsy(mock.history.get[0].headers.Authorization);
+});
